1c99c51357b9a8f89905d50ec85a9f484b3389e7
Ryo Wheatley <ryo.capricorn92@gmail.com>
2017-10-15 22:06:02 -0400

    clean and lint index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+/* eslint-disable react/jsx-filename-extension */
+/* global document */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'font-awesome/css/font-awesome.css';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+/* eslint-enable */
