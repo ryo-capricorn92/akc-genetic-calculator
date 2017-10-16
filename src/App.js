0d9d19e04c276ae8c32f74be7bd18a7449438654
Ryo Wheatley <ryo.capricorn92@gmail.com>
2017-10-15 20:18:46 -0400

    add showcase component for main page splash

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import styled, { keyframes } from 'styled-components';
 import logo from './logo.svg';
 
 import Header from './presentation/Header';
+import Showcase from './presentation/Showcase';
 import Splash from './presentation/Splash';
+
+const ShowcaseContainer = styled.div.withConfig({
+  displayName: 'ShowcaseContainer',
+})`
+  display: flex;
+  padding: 35px;
+  justify-content: center;
+
+  & section + section {
+    margin-left: 35px;
+  }
+`;
 
 class App extends Component {
   render() {
@@ -12,6 +25,17 @@ class App extends Component {
       <div>
         <Header />
         <Splash>A slim and trim genetic calculator<br />to make your life just a little easier</Splash>
+        <ShowcaseContainer>
+          <Showcase>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas auctor, nisi et pharetra fermentum, massa augue semper purus, consequat mollis felis diam eu tortor. Suspendisse potenti. Curabitur id dapibus metus. Quisque et lectus quis ipsum fermentum porta. Duis blandit elementum diam, quis viverra sapien mattis feugiat. Suspendisse potenti. Fusce tincidunt augue tortor, a auctor nisl finibus sit amet. Sed neque ipsum, lobortis in vehicula at, dapibus id massa. Quisque consectetur iaculis neque in vestibulum. Mauris risus libero, maximus vestibulum scelerisque ut, suscipit sit amet justo. Cras at ex eget lectus rutrum laoreet sit amet sit amet metus.
+          </Showcase>
+          <Showcase>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas auctor, nisi et pharetra fermentum, massa augue semper purus, consequat mollis felis diam eu tortor. Suspendisse potenti. Curabitur id dapibus metus. Quisque et lectus quis ipsum fermentum porta. Duis blandit elementum diam, quis viverra sapien mattis feugiat. Suspendisse potenti. Fusce tincidunt augue tortor, a auctor nisl finibus sit amet. Sed neque ipsum, lobortis in vehicula at, dapibus id massa. Quisque consectetur iaculis neque in vestibulum. Mauris risus libero, maximus vestibulum scelerisque ut, suscipit sit amet justo. Cras at ex eget lectus rutrum laoreet sit amet sit amet metus.
+          </Showcase>
+          <Showcase>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas auctor, nisi et pharetra fermentum, massa augue semper purus, consequat mollis felis diam eu tortor. Suspendisse potenti. Curabitur id dapibus metus. Quisque et lectus quis ipsum fermentum porta. Duis blandit elementum diam, quis viverra sapien mattis feugiat. Suspendisse potenti. Fusce tincidunt augue tortor, a auctor nisl finibus sit amet. Sed neque ipsum, lobortis in vehicula at, dapibus id massa. Quisque consectetur iaculis neque in vestibulum. Mauris risus libero, maximus vestibulum scelerisque ut, suscipit sit amet justo. Cras at ex eget lectus rutrum laoreet sit amet sit amet metus.
+          </Showcase>
+        </ShowcaseContainer>
       </div>
     );
   }
---
+++ src/presentation/Showcase.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.section.withConfig({
+  displayName: 'Container',
+})`
+  background-color: #eeebf4;
+  border-radius: 1px;
+  box-shadow: 1px 1px 3px rgba(86, 63, 122, 0.25);
+  color: #322547;
+  font-family: verdana;
+  font-size: 12px;
+  line-height: 1.5;
+  padding: 25px 20px;
+  position: relative;
+  text-align: justify;
+  min-width: 300px;
+  max-width: 450px;
+`;
+
+const Button = styled.button.withConfig({
+  displayName: 'Button',
+})`
+  background-color: #563f7a;
+  border: none;
+  border-radius: 50%;
+  bottom: 0;
+  color: #fff;
+  left: 50%;
+  outline: none;
+  position: absolute;
+  transform: translate(-50%, 50%);
+
+  & i {
+    margin: 1px -2px -1px 2px;
+    padding: 1px;
+  }
+`;
+
+const Showcase = ({ children, link }) => (
+  <Container>
+    { children }
+    <Button type="button">
+      <i className="fa fa-chevron-right fa-2x" />
+    </Button>
+  </Container>
+);
+
+export default Showcase;
