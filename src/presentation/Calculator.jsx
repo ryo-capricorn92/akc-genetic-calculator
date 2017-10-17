cdbf98939cccd6454bba17a052c57c9300f6fa3b
Ryo Wheatley <ryo.capricorn92@gmail.com>
2017-10-17 15:01:47 -0400

    add basic BreedList container

--- src/presentation/Calculator.jsx
+++ src/presentation/Calculator.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import BreedItem from './BreedItem';
+
+const BreedList = styled.div.withConfig({
+  displayName: 'BreedList',
+})`
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: repeat(3, 1fr);
+  padding: 25px;
+
+  @media screen and (min-width: 1350px) { grid-template-columns: repeat(4, 1fr); }
+  @media screen and (min-width: 1750px) { grid-template-columns: repeat(5, 1fr); }
+  @media screen and (min-width: 2150px) { grid-template-columns: repeat(6, 1fr); }
+`;
+
 const Calculator = () => (
-  <div>Testing</div>
+  <div>
+    <h1>Pick a breed</h1>
+    <BreedList>
+      <BreedItem
+        img="http://cdn.akc.org/akccontentimages/BreedOfficialPortraits/hero/Bull-Terrier.jpg"
+        name="Bull Terrier"
+      />
+    </BreedList>
+  </div>
 );
 
 export default Calculator;
